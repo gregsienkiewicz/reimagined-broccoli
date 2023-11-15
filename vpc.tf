980f08fd0b01e8114f66b72d8b5b3504bdda9b94
Greg Sienkiewicz <119434628+gregsienkiewicz@users.noreply.github.com>
2023-11-15 13:23:44 -0500

    fix (issue 15): Remove security gruop ingress rule allow all tcp/3000 (#16)

--- vpc.tf
+++ vpc.tf
@@ -136,14 +136,6 @@ resource "aws_security_group" "ecs" {
     security_groups = [aws_security_group.lb.id]
   }
 
-  ingress {
-    description = "Allow ALB inbound traffic"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     description = "Allow all Egress"
     from_port   = 0
